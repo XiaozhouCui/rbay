3eeba079480022152d1950dd5301b9714b917d39
Joe Cui <xiaozhou.cui@gmail.com>
2022-07-31 14:27:01 +1000

    Add logic for createItem

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,22 @@
+import { itemsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
+import { genId } from '$services/utils';
+import { serialize } from './serialize';
 
 export const getItem = async (id: string) => {};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+  const id = genId();
+
+  console.log(attrs);
+
+  // convert DateTime into unix time
+  const serialized = serialize(attrs);
+
+  await client.hSet(itemsKey(id), serialized);
+
+  return id;
+};
--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,10 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs) => {
+  return {
+    ...attrs,
+    // convert JS DateTime to unix time in milliseconds, easy to store in redis
+    createdAt: attrs.createdAt?.toMillis() || '',
+    endingAt: attrs.endingAt?.toMillis() || ''
+  };
+};
