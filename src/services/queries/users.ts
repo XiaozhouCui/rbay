1e5fc8ce24dc302684fa0cef7ecca31c3fc1c906
Joe Cui <xiaozhou.cui@gmail.com>
2022-07-31 12:52:28 +1000

    Add logic for getUserById

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,14 +5,30 @@ import { usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+  const user = await client.hGetAll(usersKey(id));
+
+  return deserilize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
   const id = genId();
-  await client.hSet(usersKey(id), {
-    username: attrs.username,
-    password: attrs.password
-  });
+  await client.hSet(usersKey(id), serialize(attrs));
 
   return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+  return {
+    username: user.username,
+    password: user.password
+  };
+};
+
+const deserilize = (id: string, user: { [key: string]: string }) => {
+  return {
+    id,
+    username: user.username,
+    password: user.password
+  };
 };
