517b8a53146d7c47b22129921290227293c602ca
Joe Cui <xiaozhou.cui@gmail.com>
2022-07-31 13:21:28 +1000

    Add logic for getSession

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,4 @@
 // "#" is used to separate redis key identifier
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
+export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,24 @@
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { Session } from '$services/types';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+  const session = await client.hGetAll(sessionsKey(id));
+
+  // if no session found, redis will return {}
+  if (Object.keys(session).length === 0) {
+    return null; // returning null means not signed in
+  }
+
+  return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+  return {
+    id,
+    userId: session.userId,
+    username: session.username
+  };
+};
