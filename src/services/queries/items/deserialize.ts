ef6599f80654e212bf16d7b979db58c164f7a914
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-01 19:11:46 +1000

    Add logic for getItem

--- src/services/queries/items/deserialize.ts
+++ src/services/queries/items/deserialize.ts
@@ -1,4 +1,19 @@
 import type { Item } from '$services/types';
 import { DateTime } from 'luxon';
 
-export const deserialize = (id: string, item: { [key: string]: string }): Item => {};
+export const deserialize = (id: string, item: { [key: string]: string }): Item => {
+  return {
+    id,
+    name: item.name,
+    ownerId: item.ownerId,
+    imageUrl: item.imageUrl,
+    description: item.description,
+    highestBidUserId: item.highestBidUserId,
+    createdAt: DateTime.fromMillis(parseInt(item.createdAt)),
+    endingAt: DateTime.fromMillis(parseInt(item.endingAt)),
+    views: parseInt(item.views),
+    likes: parseInt(item.likes),
+    bids: parseInt(item.bids),
+    price: parseFloat(item.price),
+  };
+};
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -2,16 +2,21 @@ import { itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { deserialize } from './deserialize';
 import { serialize } from './serialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+  const item = await client.hGetAll(itemsKey(id));
+
+  if (Object.keys(item).length === 0) return null;
+
+  return deserialize(id, item);
+};
 
 export const getItems = async (ids: string[]) => {};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
   const id = genId();
-
-  console.log(attrs);
 
   // convert DateTime into unix time
   const serialized = serialize(attrs);
