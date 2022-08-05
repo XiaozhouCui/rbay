7f267a90e10c1f08475fe94b20b952d0150712f8
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-05 20:35:06 +1000

    Add logic for getItems

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -15,7 +15,20 @@ export const getItem = async (id: string) => {
   return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+  // pipeline: run multiple commands at the same time
+  const commands = ids.map((id) => client.hGetAll(itemsKey(id)));
+
+  const results = await Promise.all(commands);
+
+  return results.map((result, i) => {
+    if (Object.keys(result).length === 0) {
+      return null;
+    }
+
+    return deserialize(ids[i], result);
+  });
+};
 
 export const createItem = async (attrs: CreateItemAttrs) => {
   const id = genId();
