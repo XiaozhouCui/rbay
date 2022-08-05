96b652a79957ce4261581c7e866a0aea50ae4f40
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-05 20:18:37 +1000

    Run pipeline in sandbox

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,25 +2,30 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	// set hash in redis: HSET car color red year 1950
-	await client.hSet('car', {
-		color: 'red',
-		year: 1950,
-		engine: { clylinders: 8 },
-		owner: null || '', // node-redis cannot process 'null' value
-		service: undefined || ''
-	});
+  // set hash in redis: HSET car color red year 1950
+  await client.hSet('car1', {
+    color: 'red',
+    year: 1950,
+    owner: null || '', // node-redis cannot process 'null' value
+    service: undefined || '',
+  });
+  await client.hSet('car2', {
+    color: 'green',
+    year: 1955,
+  });
+  await client.hSet('car3', {
+    color: 'blue',
+    year: 1960,
+  });
 
-	const car = await client.hGetAll('car');
+  // pipeline: run multiple commands at the same time
+  const commands = [1, 2, 3].map((id) => client.hGetAll(`car${id}`));
 
-	// when looking for a non-exist hash, it will return an empty object instead of null
-	// const car = await client.hGetAll('car#5ho8f1'); // result is {}
+  const results = await Promise.all(commands);
 
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
+  console.log(results);
 
-	console.log(car);
+  // when looking for a non-exist hash, it will return an empty object instead of null
+  // const car = await client.hGetAll('car#5ho8f1'); // result is {}
 };
 run();
