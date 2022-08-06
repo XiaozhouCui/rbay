0cd1b9c61e4f691a9937396212ceb8c9b4626c3f
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-06 13:32:10 +1000

    Add logic for likes count

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,4 +1,4 @@
-import { userLikesKey } from '$services/keys';
+import { itemsKey, userLikesKey } from '$services/keys';
 import { client } from '$services/redis';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
@@ -9,13 +9,25 @@ export const userLikesItem = async (itemId: string, userId: string) => {
 export const likedItems = async (userId: string) => {};
 
 export const likeItem = async (itemId: string, userId: string) => {
-  // add to redis likes set: SADD users:likes#userId itemId
-  await client.sAdd(userLikesKey(userId), itemId);
+  // SADD users:likes#userId itemId (should return 1)
+  const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+  // if inserted successfully (inserted === 1)
+  if (inserted) {
+    // increase items hash 'likes' by 1: HINCRBY items#idemId likes 1
+    return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+  }
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-  // remove from redis likes set: SREM users:likes#userId itemId
-  await client.sRem(userLikesKey(userId), itemId);
+  // SREM users:likes#userId itemId (should return 1)
+  const removed = await client.sRem(userLikesKey(userId), itemId);
+
+  // if removed successfully (removed === 1)
+  if (removed) {
+    // HINCRBY items#idemId likes -1
+    return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+  }
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
