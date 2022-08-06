903bbbc6d1c7c3b770166b27a978172fc54731f2
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-06 12:46:00 +1000

    Requiring unique usernames

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,35 +1,46 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {
-	const user = await client.hGetAll(usersKey(id));
+  const user = await client.hGetAll(usersKey(id));
 
-	return deserialize(id, user);
+  return deserialize(id, user);
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-	const id = genId();
+  const id = genId();
 
-	await client.hSet(usersKey(id), serialize(attrs));
+  // see if the usuername is already in the set of unsernames
+  // redis: SISMEMBER
+  const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
 
-	return id;
+  // if so, throw an error
+  if (exists) {
+    throw new Error('Username is taken');
+  }
+
+  // otherwise, continue to create user account
+  await client.hSet(usersKey(id), serialize(attrs));
+  await client.sAdd(usernamesUniqueKey(), attrs.username);
+
+  return id;
 };
 
 const serialize = (user: CreateUserAttrs) => {
-	return {
-		username: user.username,
-		password: user.password
-	};
+  return {
+    username: user.username,
+    password: user.password,
+  };
 };
 
 const deserialize = (id: string, user: { [key: string]: string }) => {
-	return {
-		id,
-		username: user.username,
-		password: user.password
-	};
+  return {
+    id,
+    username: user.username,
+    password: user.password,
+  };
 };
