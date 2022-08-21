b2f20a7d0f4a11e9c246abb42fd60577e90a8caf
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-21 12:52:16 +1000

    Add logic for incrementView

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,11 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const incrementView = async (itemId: string, userId: string) => {
+  return Promise.all([
+    // update the item's hash, increasse item's views by 1
+    client.hIncrBy(itemsKey(itemId), 'views', 1),
+    // update the 'items:views' sorted set, increasse item's views by 1
+    client.zIncrBy(itemsByViewsKey(), 1, itemId),
+  ]);
+};
