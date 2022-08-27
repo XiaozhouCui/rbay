5ad9ecf1425955031e6f7a13262ae3f579c421fc
Joe Cui <xiaozhou.cui@gmail.com>
2022-08-27 11:37:00 +1000

    Load relational data from a sorted set

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -7,6 +7,7 @@ export const itemsByEndingTime = async (
   offset = 0, // number to skip
   count = 10
 ) => {
+  // get sorted set of item ids by ending time
   const ids = await client.zRange(
     itemsByEndingAtKey(),
     // unix time from now to infinity
@@ -21,5 +22,9 @@ export const itemsByEndingTime = async (
     }
   );
 
-  console.log(ids);
+  // loop through all item ids to get full item details (item hashes) as serialized data
+  const results = await Promise.all(ids.map((id) => client.hGetAll(itemsKey(id))));
+
+  // return the deserialized items
+  return results.map((item, i) => deserialize(ids[i], item));
 };
