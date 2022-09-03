ba48e68417777d74745110ad2ad50c558b2370a2
Joe Cui <xiaozhou.cui@gmail.com>
2022-09-03 22:11:47 +1000

    Create and retrieve bid history

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,12 +1,25 @@
+import { bidHistoryKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateBidAttrs } from '$services/types';
 import { DateTime } from 'luxon';
 
-// Add a new record into the bid history LIST
-export const createBid = async (attrs: CreateBidAttrs) => {};
+// Add a new record into the item's bid history LIST
+export const createBid = async (attrs: CreateBidAttrs) => {
+  const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+  // RPUSH history#a1 25:16612345095
+  return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 // Returns bid history for a single item in order of increasing time
 export const getBidHistory = async (itemId: string, offset = 0, count = 10) => {
-  return [];
+  // in Redis LIST, -1 is the index of last item, 0 is first
+  const startIndex = -1 * offset - count;
+  const endIndex = -1 - offset;
+
+  // LRANGE history#a1 -10 -1, get the latest 10 bids
+  const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+
+  return range.map((bid) => deserializeHistory(bid));
 };
 
 const serializeHistory = (amount: number, createdAt: number) => {
